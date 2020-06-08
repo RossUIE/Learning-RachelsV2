fc685a91043e5a2ded7bc44be2a3d3166b43a47f
Ross <r.mcmurray1@hotmail.co.uk>
2020-06-08 21:18:29 +0100

    Navbar color change on scroll

--- js/main.js
+++ js/main.js
@@ -105,9 +105,24 @@ const formValidation = (event) => {
     }
 }
 
+const navScroll = () => {
+    var navbar = document.querySelector('nav')
+
+    window.onscroll = function() {
+
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 0) {
+        navbar.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+    }
+    }
+}
+
 const app = () => {
     navSlide();
     handleMap();
+    navScroll();
 }
 
 app();
