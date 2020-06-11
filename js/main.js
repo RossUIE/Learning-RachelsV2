03e4b5d4b684d1fdc9008523f69ac490567c65cb
Ross <r.mcmurray1@hotmail.co.uk>
2020-06-11 20:42:25 +0100

    General styling fixes

--- js/main.js
+++ js/main.js
@@ -198,26 +198,11 @@ const app = () => {
 app();
 
 $(document).ready(function() {
-	$('.c-gallery-content').magnificPopup({
+	$('.c-gallery-content, .room-body').magnificPopup({
 		delegate: 'a',
 		type: 'image',
 		tLoading: 'Loading image #%curr%...',
 		mainClass: 'mfp-img-mobile',
-		zoom: {
-			enabled: true, // By default it's false, so don't forget to enable it
-		
-			duration: 300, // duration of the effect, in milliseconds
-			easing: 'ease-in-out', // CSS transition easing function
-		
-			// The "opener" function should return the element from which popup will be zoomed in
-			// and to which popup will be scaled down
-			// By defailt it looks for an image tag:
-			opener: function(openerElement) {
-			  // openerElement is the element on which popup was initialized, in this case its <a> tag
-			  // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-			  return openerElement.is('img') ? openerElement : openerElement.find('img');
-			}
-		},
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
