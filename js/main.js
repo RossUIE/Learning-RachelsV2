18e15a331c608377673ef712fd47e859a66cad33
Ross <r.mcmurray1@hotmail.co.uk>
2020-06-11 19:55:58 +0100

    Gallery changes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.c-header_links');
-	const navLinks = document.querySelectorAll('li');
+	const navLinks = document.querySelectorAll('.c-header li');
 	const backdrop = document.querySelector('.c-backdrop');
 
 	burger.addEventListener('click', ()=> {
@@ -36,11 +36,14 @@ const navSlide = () => {
 			nav.classList.remove('nav-active')
 			backdrop.classList.remove('backdrop-active');
 			navLinks.forEach((link, index)=> {
-				if(link.style.animation) {
-					link.style.animation = ''
-				} else {
-					link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`
-				}   
+				if (screen.width <= 659) {
+					if(link.style.animation) {
+						link.style.animation = ''
+					} else {
+						link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`
+					}  
+				}
+				 
 			})
 			burger.classList.toggle('toggle')
 		  })
@@ -193,3 +196,38 @@ const app = () => {
 }
 
 app();
+
+$(document).ready(function() {
+	$('.c-gallery-content').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+		
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-in-out', // CSS transition easing function
+		
+			// The "opener" function should return the element from which popup will be zoomed in
+			// and to which popup will be scaled down
+			// By defailt it looks for an image tag:
+			opener: function(openerElement) {
+			  // openerElement is the element on which popup was initialized, in this case its <a> tag
+			  // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+			  return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+		},
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return '<small>Learning@Rachels</small>';
+			}
+		}
+	});
+});
